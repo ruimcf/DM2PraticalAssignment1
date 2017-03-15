27a7811546b94e2fc9ac4bc2387821c94800603c
Rui Fonseca <ruimcf95@gmail.com>
2017-03-15 18:59:46 +0000

    guide extraction

---
+++ main.r
@@ -0,0 +1,18 @@
+library(readxl)
+
+read_excel_allsheets <- function(filename) {
+  sheets <- readxl::excel_sheets(filename)
+  x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
+  names(x) <- sheets
+  x
+}
+
+mysheets <- read_excel_allsheets("Road-Accident-Safety-Data-Guide.xls")
+
+
+removeNA <- function(data.frame.list) {
+  x <- lapply(names(data.frame.list), function(X) data.frame.list[[X]][!is.na(data.frame.list[[X]])])
+  x
+}
+
+mysheets <- removeNA(mysheets)
