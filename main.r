b64e97e2a819356110c14cdce283f2fd6b761007
pedrocb <pedrocb_c-s@hotmail.com>
2017-03-16 18:43:22 +0000

    convert date and time

--- main.r
+++ main.r
@@ -1,8 +1,9 @@
 library(readxl)
+library(lubridate)
 
 read_excel_allsheets <- function(filename) {
   sheets <- readxl::excel_sheets(filename)
-  #sheets <- sheets[3:length(sheets)]
+  sheets <- sheets[3:length(sheets)]
   x <- lapply(sheets, function(X) readxl::read_excel(filename, sheet = X))
   names(x) <- sheets
   x
@@ -19,4 +20,22 @@ convert_codes <- function(df, col_name, excel_section_name) {
 
 # Example
 small_sample <- head(data, n = 200)
-sample_with_label <- convert_codes(small_sample, "Police_Force", "Police Force")
+sample_with_label <- convert_codes(small_sample, "Accident_Severity", "Accident Severity")
+sample_with_label <- convert_codes(sample_with_label, "Police_Force", "Police Force")
+
+convert_date_and_time <- function(df) {
+  df$Date <- as.Date(df$Date, "%d/%m/%Y")
+  df$Day <- day(df$Date)               
+  df$Month <- month(df$Date)
+  df$Date <- NULL
+  
+  df$Time <- hm(df$Time)
+  df$Hour <- hour(df$Time)
+  df$Minute <- minute(df$Time)
+  df$Time <- NULL
+  df
+}
+
+useless <- c("Accident_Index", "Location_Easting_OSGR", "Location_Northing_OSGR")
+data.discretized <- data[, !names(data) %in% useless]
+data.discretized$Police_Force <- factor(data.discretized$Police_Force)
